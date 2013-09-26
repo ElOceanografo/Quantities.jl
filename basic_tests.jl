342f7f61000750a01969757b974e92a917dcdf50
Sam Urmy <sam@Thysanoessa>
2013-09-26 01:51:23 -0400

    blank out QuantityArrays for now

--- basic_tests.jl
+++ basic_tests.jl
@@ -1,11 +1,11 @@
-# require("Quantities")
-# using Base.Test
-# using Quantities
+require("Quantities")
+using Base.Test
+using Quantities
 include("./src/quantities.jl")
 meter = Unit(Length, "m")
 second = Unit(Time, "s")
 
 g = Quantity(9.81, [meter, second], [1, -2])
-arr = QuantityArray([1:5], [meter, second], [1, -1])
+# arr = QuantityArray([1:5], [meter, second], [1, -1])
 
 println(g)
